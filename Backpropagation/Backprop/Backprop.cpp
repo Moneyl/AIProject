913788f6a22c5e42be99626d0a07642f1808faf6
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-09-28 15:27:13 -0400

    Add more code comments

--- Backpropagation/Backprop/Backprop.cpp
+++ Backpropagation/Backprop/Backprop.cpp
@@ -9,17 +9,18 @@ int main()
     //Seed random number generator. Used for initial neuron weights
     srand(time(NULL));
 
-    //Create a neural net
+    //Create an empty neural net
     NeuralNet net;
 
-    int numInputs = 2;
+    //Neural net config
+    int numInputs = 2; //Note: Must match number of inputs in input data entries
+    int numOutputs = 2; //Note: Must match number of outputs in output data entries
     int numHiddenNeurons = 1;
-    int numOutputNeurons = 2;
-    int numTrainingRuns = 2000;
+    int numTrainingRuns = 20000;
 
     //Add layers to neural net
-    net.Layers.push_back(Layer(numHiddenNeurons, numInputs));
-    net.Layers.push_back(Layer(numOutputNeurons, numHiddenNeurons));
+    net.Layers.push_back(Layer(numHiddenNeurons, numInputs)); //Hidden layer
+    net.Layers.push_back(Layer(numOutputs, numHiddenNeurons)); //Output layer
     
     //Todo: Test network with larger data set
     //Todo: Find real data to test the on the network. This is just mock data with no meaning
@@ -27,7 +28,7 @@ int main()
     std::vector<float> expectedOutputData = { 0.0f, 0.5f };
 
     //Output initial neuron values
-    printf("Created a neural network with %d inputs, %d hidden neurons, and %d output neurons.\n", numInputs, numHiddenNeurons, numOutputNeurons);
+    printf("Created a neural network with %d inputs, %d hidden neurons, and %d output neurons.\n", numInputs, numHiddenNeurons, numOutputs);
     Layer& hiddenLayer = net.Layers[0];
     Layer& outputLayer = net.Layers[1];
     printf("Hidden layer neurons:\n");
--- Backpropagation/Backprop/NeuralNet.h
+++ Backpropagation/Backprop/NeuralNet.h
@@ -37,13 +37,13 @@ public:
 class NeuralNet
 {
 public:
-    //Forward propagate input values through the network
+    //Forward propagate input values through the network. Returns network output.
     std::vector<float> ForwardPropagate(const std::vector<float>& inputBase);
-    //Backpropagate output error through network by comparing expected outputs to generated outputs
+    //Backpropagate output error through network by comparing expected outputs to calculated outputs
     void Backpropagate(const std::vector<float>& expectedOutputs);
-    //Update the weights of neurons
+    //Update the weights and biases of neurons
     void UpdateNeuronWeights(const std::vector<float>& inputBase);
-    //Train the network by repeated forward propogating an input, 
+    //Train the network by repeatedly forward propogating an input, 
     //then using backward propagation to update weights and biases by comparing output to expected output
     void Train(std::vector<std::vector<float>> inputs, std::vector<std::vector<float>> expectedOutputs, int numTrainingRuns);
 
